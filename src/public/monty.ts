6c922c3bd392d46235db04d12b88c00432680af2
jw5374 <jw5374@nyu.edu>
2022-05-21 12:55:44 -0400

    adding stats per session

--- src/public/monty.ts
+++ src/public/monty.ts
@@ -1,6 +1,7 @@
 const mainWindow = document.querySelector('main')
 const doorContainer = document.getElementById('hall-container')
 const doors: HTMLCollectionOf<Element> = document.getElementsByClassName('door')
+const stats = document.getElementById('stat-row')?.children
 const message = document.getElementById('message')
 const replayButton = document.getElementById('replay-button')
 const startButton = document.getElementById('start-button')
@@ -42,11 +43,15 @@ for(let door of doors) {
         if(!e.target) {
             return
         }
+        if(!stats) {
+            return
+        }
         let targetEl = e.target as HTMLElement
         targetEl.parentElement?.classList.add('outline')
         targetEl.classList.add('active')
         doorClicked = parseInt(targetEl.dataset.doornum!)
         if(!clicked) {
+            doorNum = doorClicked;
             (doors[Math.floor(Math.random() * 3)] as HTMLElement).dataset.isPrize = "true"
             let randomChoice = Math.floor(Math.random() * 3)
             let randomDoor = doors[randomChoice] as HTMLElement
@@ -60,6 +65,11 @@ for(let door of doors) {
             message?.classList.remove("hide")
             clicked = 1
         } else {
+            let statArray: number[] = []
+            for(let stat of stats) {
+                statArray.push(parseFloat(stat.textContent!))
+            }
+            statArray[0]++
             let win: HTMLParagraphElement = document.createElement('p')
             targetEl.parentElement?.classList.add('outline')
             targetEl.classList.add('doorOpened')
@@ -67,12 +77,29 @@ for(let door of doors) {
             if(targetEl.dataset.isPrize == "true") {
                 targetEl.parentElement!.classList.add('prize-door')
                 win.textContent = "You Win a Prize!"
+                if(doorClicked == doorNum) {
+                    statArray[4]++
+                } else {
+                    statArray[3]++
+                }
             } else {
+                if(doorClicked == doorNum) {
+                    statArray[6]++
+                } else {
+                    statArray[5]++
+                }
                 win.textContent = "Congratulations on a goat!"
             }
+            statArray[1] = statArray[5] + statArray[3]
+            statArray[2] = statArray[6] + statArray[4]
+            statArray[7] = statArray[3] / statArray[1]
+            statArray[8] = statArray[4] / statArray[2]
             doorContainer?.appendChild(win)
             doorContainer?.classList.add('disable')
             replayButton?.classList.remove('hide')
+            for(let i = 0; i < stats.length; i++) {
+                stats[i].textContent = statArray[i].toString()
+            }
         }
     })
 }
